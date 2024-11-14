273f25099bbecf84558995e89f3e081306e590cc
Solovey Roman <gasoss221510@gmail.com>
2024-11-14 20:57:09 +0300

    fix: research when category or categories list changed

--- src/components/sidebar/categories/categories-list/categories-list.component.ts
+++ src/components/sidebar/categories/categories-list/categories-list.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { UpdateCategoryModalComponent } from '../../../modals/update-category/update-category.components';
 import { CategoryDto } from '../dto';
 import { CategoryService, ToastService } from '../../../../libs/services';
@@ -7,7 +13,7 @@ import { toObservable } from '@angular/core/rxjs-interop';
 import { DeleteCategoryModalComponent } from '../../../modals/delete-category/delete-category.component';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, map, of, switchMap } from 'rxjs';
+import { debounceTime, map, of, Subscription, switchMap } from 'rxjs';
 import { SEARCH_DEBOUNCE_TIME } from '../../../../libs/helpers';
 import { ToastComponent } from '../../../toast/toast.component';
 import { SpinnerComponent } from '../../../spinner/spinner.component';
@@ -27,9 +33,12 @@ import { SpinnerColorsEnum } from '../../../../libs/enums';
   ],
   templateUrl: './categories-list.component.html',
 })
-export class CategoriesListComponent implements OnInit, AfterViewInit {
+export class CategoriesListComponent
+  implements OnInit, AfterViewInit, OnDestroy
+{
   selectedCategory?: CategoryDto;
   @ViewChild('toast') toastComponent!: ToastComponent;
+  private subscriptions: Subscription = new Subscription();
   form: FormGroup;
   searchResults: CategoryDto[] = [];
   searchMode = false;
@@ -48,7 +57,7 @@ export class CategoriesListComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    this.form
+    const searchSubscription = this.form
       .get('search')
       ?.valueChanges.pipe(
         debounceTime(SEARCH_DEBOUNCE_TIME),
@@ -68,10 +77,23 @@ export class CategoriesListComponent implements OnInit, AfterViewInit {
         this.searchResults = [...data];
         this.searchPending = false;
       });
+
+    const categoriesSubscription = this.categories$.subscribe(() => {
+      this.form.setValue({
+        search: this.form.get('search')?.value,
+      });
+    });
+
+    this.subscriptions.add(searchSubscription);
+    this.subscriptions.add(categoriesSubscription);
   }
 
   ngAfterViewInit(): void {
     this.toastService.registerToast(this.toastComponent);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 
   onChangeButtonClick(category: CategoryDto) {
